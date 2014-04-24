98117b216c6e7d1e52ef772c44cf713900b0532f
Saumil Patel <saumilpatel1@gmail.com>
2014-04-24 11:51:39 +0200

    added service code to repository

---
+++ users.js
@@ -0,0 +1,63 @@
+function User(id, firstName, lastName) {
+	this.id = id;
+	this.firstName = firstName;
+	this.lastName = lastName;
+};
+
+function UserRepository() {
+	this.users = [];
+
+	this.createUsers = function() {
+		var numberOfUsers = 0;
+		for( var i=0; i < numberOfUsers; i++ ){
+			var id = i + 1;
+			this.users.push( new User(id, 'John ' + id, 'Doe ' + id ));
+		}
+		return this.users;
+	};
+
+	this.getMaxUserId = function() {
+		return Math.max.apply ( Math, this.users.map(function(user){
+			return users.id;
+		}));
+	};
+
+	this.getNumberOfUsers = function(){
+		return this.users.length;
+	};
+
+	this.getAll = function() {
+		return this.users;
+	};
+
+	this.getById = function() {
+		var foundUser = false;
+		for( var i=0; i < users.length; i++ ){
+			var user = users[i];
+			if ( user.id === id ){
+				foundUser = true;
+				return user;
+			}
+		}
+		if ( !foundUser ){
+			return 'User with id ' + id + ' not found.';
+		}
+	};
+
+	this.addNewUser = function(firstName, lastName ){
+		var newUser = new User(this.getMaxUserId() + 1, firstName, lastName );
+		this.users.push( newUser );
+		return this.getById( newUser.id );
+	};
+
+	this.changeUser = function( id , firstName, lastName ){
+		var user = this.getById( id );
+		user.firstName = firstName;
+		user.lastName = lastName;
+		return user;
+	};
+
+	this.deleteUser = function(id){
+		this.users.splice(id, 1 );
+	};
+};
